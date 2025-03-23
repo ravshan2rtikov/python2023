6c8f3f3ab8eb8790b957eeff5d833a5e42d23bf4
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-23 17:02:52 +0700

    created screen, 2 paddles and the ball, paddles move up so far

---
+++ ping-pong.py
@@ -0,0 +1,59 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Ping Pong")
+screen.bgcolor("Black")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+# paddle 1
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("white")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350, 0)
+
+# paddle 2
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350, 0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
+# functions
+def paddle_1_up():
+    y = paddle_1.ycor()
+    y = y + 10
+    paddle_1.sety(y)
+    if y > 240:
+        paddle_1.sety(300)
+
+def paddle_2_up():
+    y = paddle_2.ycor()
+    y = y + 10
+    paddle_2.sety(y)
+    if y > 240:
+        paddle_2.sety(300)
+
+screen.listen()
+screen.onkeypress(paddle_1_up, "w")
+screen.onkeypress(paddle_2_up, "Up")
+
+
+
+
+while True:
+      screen.update()
