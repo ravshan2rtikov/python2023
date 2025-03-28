b55f00566ff36429e420267d6a2169c4b6d5d07b
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-28 09:22:40 +0700

    scora bar works

--- ping-pong.py
+++ ping-pong.py
@@ -7,6 +7,10 @@ screen.bgcolor("Black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
+# score
+score_1 = 0
+score_2 = 0
+
 # paddle 1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
@@ -48,7 +52,7 @@ pen.write("Player 1: 0  Player 2: 0", align="center", font=("Courier", 24, "norm
 # functions
 def paddle_1_up():
     y = paddle_1.ycor()
-    y = y + 10
+    y = y + 25
     paddle_1.sety(y)
     if y > 240:
         paddle_1.sety(240)
@@ -56,7 +60,7 @@ def paddle_1_up():
 
 def paddle_1_down():
     y = paddle_1.ycor()
-    y = y - 10
+    y = y - 25
     paddle_1.sety(y)
     if y < -240:
         paddle_1.sety(-240)
@@ -64,7 +68,7 @@ def paddle_1_down():
 
 def paddle_2_up():
     y = paddle_2.ycor()
-    y = y + 10
+    y = y + 25
     paddle_2.sety(y)
     if y > 240:
         paddle_2.sety(240)
@@ -72,7 +76,7 @@ def paddle_2_up():
 
 def paddle_2_down():
     y = paddle_2.ycor()
-    y = y - 10
+    y = y - 25
     paddle_2.sety(y)
     if y < -240:
         paddle_2.sety(-240)
@@ -104,10 +108,24 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_1 += 1
+        pen.clear()
+        pen.write(
+            "Player 1: {}  Player 2: {}".format(score_1, score_2),
+            align="center",
+            font=("Courier", 24, "normal"),
+        )
 
     if ball.xcor() < -390:
         ball.setx(-290)
         ball.dx *= -1
+        score_2 += 1
+        pen.clear()
+        pen.write(
+            "Player 1: {}  Player 2: {}".format(score_1, score_2),
+            align="center",
+            font=("Courier", 24, "normal"),
+        )
 
     time.sleep(0.001)
 
